67ac694eabf135a6859700779d34d69291a90526
Pullingal Soorath <pullingalsoorath.dev@gmail.com>
2023-11-18 00:47:13 +0530

    status added

--- lib/screen/tabs/home.dart
+++ lib/screen/tabs/home.dart
@@ -27,7 +27,7 @@ class HomeScreen extends StatelessWidget {
                 ),
                 Expanded(
                   child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 14, horizontal: 10),
+                    padding: EdgeInsets.symmetric(vertical: 25, horizontal: 10),
                     child: SearchBar(
                       hintText: 'Write something here...',
                       hintStyle: MaterialStatePropertyAll(
@@ -69,17 +69,142 @@ class HomeScreen extends StatelessWidget {
                           itemCount: names.length,
                           scrollDirection: Axis.horizontal,
                           itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 5, vertical: 10),
-                              child: Container(
-                                width: 160,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  color: defaultBlue,
-                                ),
-                              ),
-                            );
+                            return index == 0
+                                ?
+//My story
+                                Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 5, vertical: 10),
+                                    child: Container(
+                                      width: 160,
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                            width: 1, strokeAlign: 0),
+                                        borderRadius: BorderRadius.circular(15),
+                                        color: Colors.white,
+                                      ),
+                                      child: Stack(
+                                        children: [
+                                          Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            children: [
+                                              Container(
+                                                height: 180,
+                                                decoration: const BoxDecoration(
+                                                  color: Colors.black,
+                                                  borderRadius:
+                                                      BorderRadius.only(
+                                                    topLeft:
+                                                        Radius.circular(15),
+                                                    topRight:
+                                                        Radius.circular(15),
+                                                  ),
+                                                ),
+                                              ),
+                                              Expanded(
+                                                child: Container(
+                                                  alignment:
+                                                      Alignment.bottomCenter,
+                                                  child: const Padding(
+                                                    padding:
+                                                        EdgeInsets.all(8.0),
+                                                    child: Text(
+                                                      'Create story',
+                                                      style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontWeight:
+                                                            FontWeight.w500,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          Column(
+                                            children: [
+                                              ClipRRect(
+                                                borderRadius:
+                                                    const BorderRadius.vertical(
+                                                  top: Radius.circular(15),
+                                                ),
+                                                child: SizedBox(
+                                                  height: 155,
+                                                  child: Image.asset(
+                                                    'assets/images/profilepic.jpeg',
+                                                    fit: BoxFit.cover,
+                                                  ),
+                                                ),
+                                              ),
+                                              CircleAvatar(
+                                                radius: 25,
+                                                backgroundColor: Colors.white,
+                                                child: Icon(
+                                                  Icons.add_circle,
+                                                  color: defaultBlue,
+                                                  size: 50,
+                                                ),
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  )
+                                :
+//Friends story
+                                Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 5, vertical: 10),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: Stack(
+                                        children: [
+                                          Container(
+                                            color: Colors.grey,
+                                            width: 160,
+                                            height: 280,
+                                            child: Image(
+                                              image: NetworkImage(
+                                                'https://picsum.photos/400?image=${index + 40}',
+                                              ),
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Stack(
+                                              alignment: Alignment.center,
+                                              children: [
+                                                Container(
+                                                  width: 50,
+                                                  height: 50,
+                                                  decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                      50,
+                                                    ),
+                                                    color: Colors.transparent,
+                                                    border: Border.all(
+                                                      width: 2.5,
+                                                      color: defaultBlue,
+                                                    ),
+                                                  ),
+                                                ),
+                                                CircleAvatar(
+                                                  radius: 20,
+                                                  backgroundColor: Colors.grey,
+                                                  backgroundImage: NetworkImage(
+                                                      'https://picsum.photos/150?image=${index + 30}'),
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  );
                           },
                         ),
                       ),
