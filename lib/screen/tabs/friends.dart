b8b21b9886f2691c4e9bebace026058ded60ea46
Pullingal Soorath <pullingalsoorath.dev@gmail.com>
2023-11-30 22:42:25 +0530

    updated

--- lib/screen/tabs/friends.dart
+++ lib/screen/tabs/friends.dart
@@ -1,10 +1,115 @@
+import 'package:facebookclone/theme/themedata.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class FriendsPage extends StatelessWidget {
   const FriendsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            // SizedBox(
+            //   width: double.infinity,
+            // ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Friends',
+                  style: TextStyle(
+                    fontFamily: 'facebook',
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SvgPicture.asset(
+                  'assets/svg/search.svg',
+                  height: 25,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[300],
+                  ),
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Suggessions'),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[300],
+                  ),
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Your Friends'),
+                ),
+              ],
+            ),
+            const Divider(
+              thickness: .5,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Friend Requests',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      '120',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'See all',
+                  style: TextStyle(color: defaultBlue),
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 1,
+                      ),
+                      child: Container(
+                        height: 80,
+                        color: Colors.red,
+                        child: Row(
+                          children: [
+                            CircleAvatar(),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
